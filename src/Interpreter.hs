738b37b3a0d3b95ea8ba49d9c4f16ab57ff9b9a6
Anès Foufa <anesfoufa@proton.me>
2024-06-21 14:28:17 +0200

    add sum and litteral list evaluation

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,15 +1,62 @@
-module Interpreter (EvaluationError (..), Evaluator, runEvaluator, evaluator) where
+module Interpreter (EvaluationError (..), Evaluator, runEvaluator, evaluator, represent, Value (..)) where
 
 import RIO
 import SExp
 
-data EvaluationError = NotImplementedYet deriving (Show, Eq)
-newtype Evaluator = Evaluator {runEvaluator :: SExp -> Either EvaluationError SExp}
+data Value
+    = VBool Bool
+    | VDouble Double
+    | VInteger Integer
+    | VString String
+    | VList [Value]
+    deriving (Eq, Show)
+
+represent :: Value -> SExp
+represent (VBool x) = SBool x
+represent (VDouble x) = SDouble x
+represent (VInteger x) = SInteger x
+represent (VString x) = SString x
+represent (VList xs) = SSExp (_listIdentifier : fmap represent xs)
+
+data EvaluationError
+    = NotImplementedYet
+    | VTypeError
+    deriving (Show, Eq)
+
+newtype Evaluator = Evaluator {runEvaluator :: SExp -> Either EvaluationError Value}
 
 evaluator :: Evaluator
 evaluator = Evaluator{runEvaluator = _defaultEvaluate}
 
-_defaultEvaluate :: SExp -> Either EvaluationError SExp
+_defaultEvaluate :: SExp -> Either EvaluationError Value
+_defaultEvaluate (SSExp (h : xs))
+    | h == _listIdentifier = VList <$> mapM _defaultEvaluate xs
+_defaultEvaluate (SSExp (h : xs))
+    | h == _sumIdentifier = _evaluateSum $ fmap _defaultEvaluate xs
 _defaultEvaluate (SSExp _) = Left NotImplementedYet
 _defaultEvaluate (SId _) = Left NotImplementedYet
-_defaultEvaluate sexp = Right sexp
+_defaultEvaluate (SBool x) = Right (VBool x)
+_defaultEvaluate (SDouble x) = Right (VDouble x)
+_defaultEvaluate (SInteger x) = Right (VInteger x)
+_defaultEvaluate (SString x) = Right (VString x)
+
+_evaluateSum :: [Either EvaluationError Value] -> Either EvaluationError Value
+_evaluateSum = foldr f (Right (VInteger 0))
+  where
+    f (Right v) _ | isNotNumeric v = Left VTypeError
+    f (Left ee) _ = Left ee
+    f _ (Left ee) = Left ee
+    f (Right (VInteger a)) (Right (VInteger b)) = Right (VInteger (a + b))
+    f (Right (VDouble a)) (Right (VDouble b)) = Right (VDouble (a + b))
+    f (Right (VInteger a)) (Right (VDouble b)) = Right (VDouble (fromIntegral a + b))
+    f (Right (VDouble a)) (Right (VInteger b)) = Right (VDouble (a + fromIntegral b))
+    f _ _ = Left VTypeError
+    isNotNumeric (VInteger _) = False
+    isNotNumeric (VDouble _) = False
+    isNotNumeric _ = True
+
+_listIdentifier :: SExp
+_listIdentifier = SId (Identifier{id = "list"})
+
+_sumIdentifier :: SExp
+_sumIdentifier = SId (Identifier{id = "+"})
--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -1,6 +1,7 @@
 module InterpreterSpec (spec) where
 
-import Interpreter (EvaluationError (..), evaluator, runEvaluator)
+import Data.List (repeat)
+import Interpreter (EvaluationError (..), Value (..), evaluator, runEvaluator)
 import RIO
 import SExp
 import Test.Hspec
@@ -9,13 +10,33 @@ import Test.Hspec.QuickCheck
 spec :: Spec
 spec = do
     describe "evaluator" do
+        let listIdentifier = SId (Identifier{id = "list"})
+        let sumIndentifier = SId (Identifier{id = "+"})
         prop "evaluates S Integer as S Integer" do
-            \i -> runEvaluator evaluator (SInteger i) `shouldBe` Right (SInteger i)
+            \i -> runEvaluator evaluator (SInteger i) `shouldBe` Right (VInteger i)
         prop "evaluates S Double as S Double" do
-            \i -> runEvaluator evaluator (SDouble i) `shouldBe` Right (SDouble i)
+            \i -> runEvaluator evaluator (SDouble i) `shouldBe` Right (VDouble i)
         prop "evaluates S String as S String" do
-            \i -> runEvaluator evaluator (SString i) `shouldBe` Right (SString i)
+            \i -> runEvaluator evaluator (SString i) `shouldBe` Right (VString i)
         prop "Does not evaluate S Identifier" do
             \i -> runEvaluator evaluator (SId (Identifier{id = i})) `shouldBe` Left NotImplementedYet
+        it "Evaluates list prefixed as lists" do
+            runEvaluator evaluator (SSExp [listIdentifier]) `shouldBe` Right (VList [])
         prop "Does not evaluate S Lists" do
             \i -> runEvaluator evaluator (SSExp i) `shouldBe` Left NotImplementedYet
+        it "Does not evaluate empty list" do
+            runEvaluator evaluator (SSExp []) `shouldBe` Left NotImplementedYet
+        it "Does not evaluate list with identifier" do
+            runEvaluator evaluator (SSExp [SId (Identifier{id = "foo"})]) `shouldBe` Left NotImplementedYet
+        it "Evaluates empty sum as 0" do
+            runEvaluator evaluator (SSExp [sumIndentifier]) `shouldBe` Right (VInteger 0)
+        prop "should evaluate sum of integers as sum of integers" do
+            \is -> runEvaluator evaluator (SSExp (sumIndentifier : fmap SInteger is)) `shouldBe` Right (VInteger (sum is))
+        prop "Evaluates sum of doubles" do
+            \ds -> isRight (runEvaluator evaluator (SSExp (sumIndentifier : fmap SDouble ds))) `shouldBe` True
+        it "Returns type errors when summing non numerics" do
+            runEvaluator evaluator (SSExp [sumIndentifier, SInteger 1, SString "foo"]) `shouldBe` Left VTypeError
+        it "Evaluates sums lazily on type errors" do
+            runEvaluator evaluator (SSExp (sumIndentifier : SInteger 5 : repeat (SString "foo"))) `shouldBe` Left VTypeError
+        it "Evaluates nested sums" do
+            runEvaluator evaluator (SSExp [sumIndentifier, SSExp [sumIndentifier, SInteger 1, SInteger 2], SInteger 3, SInteger 4]) `shouldBe` Right (VInteger 10)
