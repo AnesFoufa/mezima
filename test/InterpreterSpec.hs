6f111b8ca5c8312bae94fd10cc368fdd9947aefb
Anès Foufa <anesfoufa@proton.me>
2024-06-25 10:21:00 +0200

    test the evaluation of the equality operator

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -11,7 +11,8 @@ spec :: Spec
 spec = do
     describe "evaluator" do
         let listIdentifier = SId (Identifier{id = "list"})
-        let sumIndentifier = SId (Identifier{id = "+"})
+        let sumIdentifier = SId (Identifier{id = "+"})
+        let equalityIdentifier = SId (Identifier{id = "="})
         prop "evaluates S Integer as S Integer" do
             \i -> runEvaluator evaluator (SInteger i) `shouldBe` Right (VInteger i)
         prop "evaluates S Double as S Double" do
@@ -29,14 +30,32 @@ spec = do
         it "Does not evaluate list with identifier" do
             runEvaluator evaluator (SSExp [SId (Identifier{id = "foo"})]) `shouldBe` Left NotImplementedYet
         it "Evaluates empty sum as 0" do
-            runEvaluator evaluator (SSExp [sumIndentifier]) `shouldBe` Right (VInteger 0)
+            runEvaluator evaluator (SSExp [sumIdentifier]) `shouldBe` Right (VInteger 0)
         prop "should evaluate sum of integers as sum of integers" do
-            \is -> runEvaluator evaluator (SSExp (sumIndentifier : fmap SInteger is)) `shouldBe` Right (VInteger (sum is))
+            \is -> runEvaluator evaluator (SSExp (sumIdentifier : fmap SInteger is)) `shouldBe` Right (VInteger (sum is))
         prop "Evaluates sum of doubles" do
-            \ds -> isRight (runEvaluator evaluator (SSExp (sumIndentifier : fmap SDouble ds))) `shouldBe` True
+            \ds -> isRight (runEvaluator evaluator (SSExp (sumIdentifier : fmap SDouble ds))) `shouldBe` True
         it "Returns type errors when summing non numerics" do
-            runEvaluator evaluator (SSExp [sumIndentifier, SInteger 1, SString "foo"]) `shouldBe` Left VTypeError
+            runEvaluator evaluator (SSExp [sumIdentifier, SInteger 1, SString "foo"]) `shouldBe` Left VTypeError
         it "Evaluates sums lazily on type errors" do
-            runEvaluator evaluator (SSExp (sumIndentifier : SInteger 5 : repeat (SString "foo"))) `shouldBe` Left VTypeError
+            runEvaluator evaluator (SSExp (sumIdentifier : SInteger 5 : repeat (SString "foo"))) `shouldBe` Left VTypeError
         it "Evaluates nested sums" do
-            runEvaluator evaluator (SSExp [sumIndentifier, SSExp [sumIndentifier, SInteger 1, SInteger 2], SInteger 3, SInteger 4]) `shouldBe` Right (VInteger 10)
+            runEvaluator evaluator (SSExp [sumIdentifier, SSExp [sumIdentifier, SInteger 1, SInteger 2], SInteger 3, SInteger 4]) `shouldBe` Right (VInteger 10)
+        it "Evaluates equality without arguments as True" do
+            runEvaluator evaluator (SSExp [equalityIdentifier]) `shouldBe` Right (VBool True)
+        prop "Evaluates equality of similar integers as True" do
+            \(i, n) -> runEvaluator evaluator (SSExp (equalityIdentifier : replicate i (SInteger n))) `shouldBe` Right (VBool True)
+        prop "Evaluates equality of similar doubles as True" do
+            \(i, d) -> runEvaluator evaluator (SSExp (equalityIdentifier : replicate i (SDouble d))) `shouldBe` Right (VBool True)
+        it "Evaluates equal integers and and doubles as True" do
+            runEvaluator evaluator (SSExp [equalityIdentifier, SDouble 1.0, SInteger 1]) `shouldBe` Right (VBool True)
+        it "Evaluates equality of different integers as False" do
+            runEvaluator evaluator (SSExp [equalityIdentifier, SInteger 1, SInteger 2]) `shouldBe` Right (VBool False)
+        it "Evaluates equality of different doubles as False" do
+            runEvaluator evaluator (SSExp [equalityIdentifier, SDouble 1.2, SDouble 2.4]) `shouldBe` Right (VBool False)
+        it "Evaluates equality of non numeric as VTypeError" do
+            runEvaluator evaluator (SSExp [equalityIdentifier, SString "foo"]) `shouldBe` Left VTypeError
+        it "Lazily evaluates an equality returning VTypeError" do
+            runEvaluator evaluator (SSExp (equalityIdentifier : SString "foo" : repeat (SInteger 42))) `shouldBe` Left VTypeError
+        it "Lazily evaluates an equality returning False" do
+            runEvaluator evaluator (SSExp (equalityIdentifier : SInteger 1 : SInteger 2 : repeat (SString "foo"))) `shouldBe` Right (VBool False)
