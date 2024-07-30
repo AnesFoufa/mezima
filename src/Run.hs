3b2fe463fe19faa28583a6c8abff6efec561b2b3
Anès Foufa <anesfoufa@proton.me>
2024-07-30 13:22:27 +0200

    feat: use haskeline for repl

--- src/Run.hs
+++ src/Run.hs
@@ -6,30 +6,32 @@ import Control.Monad.State (runState)
 import Interpreter
 import RIO
 import SExp
+import System.Console.Haskeline (InputT, defaultSettings, getInputLine, outputStrLn, runInputT)
 import Text.Megaparsec (errorBundlePretty, parse)
 import Types
-import Prelude (getLine)
+
+repl :: SymbolsTable -> InputT IO ()
+repl state = do
+    mInput <- getInputLine "% "
+    let input = fromMaybe "" mInput
+    let res = parse sexpParser "" input
+    case res of
+        Left eb -> do
+            outputStrLn $ errorBundlePretty eb
+            repl state
+        Right sexp -> do
+            outputStrLn $ show sexp
+            let (evaluation, state') = runState (runEvaluator evaluator sexp) state
+            case evaluation of
+                Left ee -> outputStrLn $ show ee
+                Right v -> outputStrLn $ show $ represent v
+            repl state'
 
 run :: RIO App ()
 run = do
     env <- ask
     let options = appOptions env
     let suffix = if optionsVerbose options then " Verbose !!!" else ""
-    logInfo $ "We're inside the application!" <> suffix
+    logInfo $ "You are entering Mezima's REPL. " <> suffix
     let initState = [mempty]
-    repl initState
-  where
-    repl state = do
-        line <- liftIO getLine
-        let res = parse sexpParser "" line
-        case res of
-            Left be -> do
-                logInfo $ fromString $ errorBundlePretty be
-                repl state
-            Right sexp -> do
-                logInfo $ fromString $ show sexp
-                let (result, nState) = runState (runEvaluator evaluator sexp) state
-                case result of
-                    Left ee -> logInfo $ fromString $ show ee
-                    Right v -> logInfo $ fromString $ show $ represent v
-                repl nState
+    liftIO $ runInputT defaultSettings $ repl initState
